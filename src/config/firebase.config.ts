23880f2ba5aa5ab43bdee9084abf128617b6edee
jcksnfales <jcksnfales@gmail.com>
2023-06-14 19:44:37 -0400

    Added firebase and react-redux

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCCqHfR6yRz_PI5-QzzpFoyShGu3UiV3tE",
+  authDomain: "ct-react-book-catalogue.firebaseapp.com",
+  projectId: "ct-react-book-catalogue",
+  storageBucket: "ct-react-book-catalogue.appspot.com",
+  messagingSenderId: "250069419374",
+  appId: "1:250069419374:web:3b4aa6c7364c0a38482d7f"
+};
