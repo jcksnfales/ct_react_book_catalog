6b028eed1fb170d84cde82918604f22bf975d5e5
jcksnfales <jcksnfales@gmail.com>
2023-06-14 21:14:31 -0400

    Minor adjustments to book table appearance

--- src/components/BookTable.tsx
+++ src/components/BookTable.tsx
@@ -4,12 +4,12 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { fetchData } from "../custom-hooks/FetchData";
 
 const columns:GridColDef[] = [
-  {field: 'title', headerName: 'Title', flex: 1},
-  {field: 'author', headerName: 'Author', flex: 1},
-  {field: 'page_count', headerName: 'Pages', flex: 1},
-  {field: 'is_hardcover', headerName: 'Hardcover?', flex: 1},
-  {field: 'isbn', headerName: 'ISBN', flex: 1},
-  {field: 'local_id', headerName: 'API ID', flex: 1},
+  {field: 'title', headerName: 'Title', flex: 5},
+  {field: 'author', headerName: 'Author', flex: 4},
+  {field: 'page_count', headerName: 'Pages', flex: 2},
+  {field: 'is_hardcover', headerName: 'Hardcover?', flex: 2},
+  {field: 'isbn', headerName: 'ISBN', flex: 3},
+  {field: 'local_id', headerName: 'API ID', flex: 3},
 ]
 
 const BookTable = () => {
@@ -19,7 +19,7 @@ const BookTable = () => {
 
 
   return (
-    <div className="container mx-auto flex flex-col w-80vw h-60vh">
+    <div className="container mx-auto mt-8 flex flex-col w-80vw h-60vh">
       <DataGrid getRowId={(row) => row.local_id} rows={bookData} columns={columns} checkboxSelection={true} onRowSelectionModelChange={(item:any) => setSelectionModel(item)}/>
     </div>
   )
