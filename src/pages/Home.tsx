d78284552f54365e3232787a90fc0ecb1c558c06
jcksnfales <jcksnfales@gmail.com>
2023-06-15 15:09:02 -0400

    Added home page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
+
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <div>
-      Home
+      <div className="mx-auto w-fit mt-6">
+        <h1 className="font-bold text-4xl text-indigo-950 font-serif">Help catalog some books!</h1>
+        <p className="mt-4 mb-5 text-center text-slate-900">We're on a journey to catalog books. Why? Because!</p>
+        <Link to="/catalog" className="block mx-auto w-fit px-4 py-2 rounded border-2 text-indigo-950 border-indigo-950 hover:text-slate-200 hover:bg-indigo-950 font-semibold tracking-wide">
+          Go to Catalog
+        </Link>
+      </div>
     </div>
   )
 }
