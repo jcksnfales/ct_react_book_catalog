b1f16bf11b9f30996d11be6032571399b2b398b7
jcksnfales <jcksnfales@gmail.com>
2023-06-15 16:16:21 -0400

    Added missing API calls

--- src/api/server.ts
+++ src/api/server.ts
@@ -11,10 +11,62 @@ export const server_calls = {
         });
 
         if (!response.ok) {
-            throw new Error('Failed to fetch data from server')
+            throw new Error('Failed to fetch data from server');
         }
 
-        return await response.json()
+        return await response.json();
+    },
+
+    create: async (data:any = {}) => {
+        const response = await fetch('https://flask-library.onrender.com/api/books', {
+            method: 'POST',
+            headers: {
+                'x-access-token': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to create new data on server');
+        }
+
+        return await response.json();
+    },
+
+    update: async (id:string, data:any = {}) => {
+        const response = await fetch(`https://flask-library.onrender.com/api/books/${id}`, {
+            method: 'POST',
+            headers: {
+                'x-access-token': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update data on server');
+        }
+
+        return await response.json();
+    },
+
+    delete: async (id:string) => {
+        const response = await fetch(`https://flask-library.onrender.com/api/books/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'x-access-token': `Bearer ${token}`,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete data on server');
+        }
+
+        return;
     }
-    // TODO - add other API methods
 }
